5186852f84dcf8be467b32279f461408c4f71cc6
gmarcovillalobos <67935262+gmarcovillalobos@users.noreply.github.com>
2026-05-26 11:02:47 -0600

    Use real React app entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 import './styles.css';
-
-function App() {
-  return (
-    <main className="compat-app">
-      <h1>DSant Cotizador</h1>
-      <p>Aplicación publicada desde el catálogo DSant Lonking.</p>
-      <a href="/">Abrir cotizador</a>
-    </main>
-  );
-}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
